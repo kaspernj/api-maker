a42130872252bde727f3896fa60b91f7b7818a76
kaspernj <kaspernj@gmail.com>
2020-04-08 14:37:04 +0200

    Removed .default

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-const Api = require("./api").default
+const Api = require("./api")
 const { CustomError, ValidationError } = require("./errors")
-const ErrorLogger = require("./error-logger").default
-const EventConnection = require("./event-connection").default
-const EventCreated = require("./event-created").default
-const EventDestroyed = require("./event-destroyed").default
-const EventEmitterListener = require("./event-emitter-listener").default
-const EventListener = require("./event-listener").default
-const EventUpdated = require("./event-updated").default
-const FormDataToObject = require("./form-data-to-object").default
-const Logger = require("./logger").default
-const ModelName = require("./model-name").default
-const Params = require("./params").default
-const Result = require("./result").default
-const SourceMapsLoader = require("./source-maps-loader").default
-const UpdatedAttribute = require("./result").default
+const ErrorLogger = require("./error-logger")
+const EventConnection = require("./event-connection")
+const EventCreated = require("./event-created")
+const EventDestroyed = require("./event-destroyed")
+const EventEmitterListener = require("./event-emitter-listener")
+const EventListener = require("./event-listener")
+const EventUpdated = require("./event-updated")
+const FormDataToObject = require("./form-data-to-object")
+const Logger = require("./logger")
+const ModelName = require("./model-name")
+const Params = require("./params")
+const Result = require("./result")
+const SourceMapsLoader = require("./source-maps-loader")
+const UpdatedAttribute = require("./result")
 
 module.exports = {
   Api,
