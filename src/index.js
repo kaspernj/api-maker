3738fc71c5cea43207f8741221a6270e69f24170
kaspernj <kaspernj@gmail.com>
2020-04-08 14:23:18 +0200

    Use require instead of import

--- src/index.js
+++ src/index.js
@@ -1,19 +1,19 @@
-import Api from "./api"
-import { CustomError, ValidationError } from "./errors"
-import ErrorLogger from "./error-logger"
-import EventConnection from "./event-connection"
-import EventCreated from "./event-created"
-import EventDestroyed from "./event-destroyed"
-import EventEmitterListener from "./event-emitter-listener"
-import EventListener from "./event-listener"
-import EventUpdated from "./event-updated"
-import FormDataToObject from "./form-data-to-object"
-import Logger from "./logger"
-import ModelName from "./model-name"
-import Params from "./params"
-import Result from "./result"
-import SourceMapsLoader from "./source-maps-loader"
-import UpdatedAttribute from "./result"
+const Api = require("./api").default
+const { CustomError, ValidationError } = require("./errors")
+const ErrorLogger = require("./error-logger").default
+const EventConnection = require("./event-connection").default
+const EventCreated = require("./event-created").default
+const EventDestroyed = require("./event-destroyed").default
+const EventEmitterListener = require("./event-emitter-listener").default
+const EventListener = require("./event-listener").default
+const EventUpdated = require("./event-updated").default
+const FormDataToObject = require("./form-data-to-object").default
+const Logger = require("./logger").default
+const ModelName = require("./model-name").default
+const Params = require("./params").default
+const Result = require("./result").default
+const SourceMapsLoader = require("./source-maps-loader").default
+const UpdatedAttribute = require("./result").default
 
 module.exports = {
   Api,
